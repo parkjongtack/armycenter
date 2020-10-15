75bd79ff3905ea238dc11055da0421cbfc846474
kangsan <to123dn@gmail.com>
2020-10-15 17:40:05 +0900

    aaa

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>국군의 날 챌린지</title>
+    <title>국군의 날 챌린지1</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/index.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
